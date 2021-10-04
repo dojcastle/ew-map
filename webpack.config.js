af92415345a56ae22c8511eefd320b256ec00983
Dojiemar Castillo <kaisercast@gmail.com>
2021-10-04 20:35:06 +0800

    File changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,8 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
-
-
-const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
-
 
 
 const config = {
@@ -17,22 +11,21 @@ const config = {
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
-        open: true,
-        host: 'localhost',
-    },
+        //....//
+        static: {                               
+          directory: path.join(__dirname, './'),  
+          watch: true
+        }
+     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -48,8 +41,6 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-        
-        config.plugins.push(new MiniCssExtractPlugin());
         
         
     } else {
