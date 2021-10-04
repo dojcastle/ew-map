7e4a3ef5bdf4f53be3baf85162d49907215ae4b8
Dojiemar Castillo <kaisercast@gmail.com>
2021-10-04 21:28:39 +0800

    js fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello World!");
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
